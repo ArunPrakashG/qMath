6e11fd566b998e93fd5bc0fa2f2670abf0088a05
Arun <arun.prakash.456789@gmail.com>
2020-07-04 00:38:15 +0530

    seperted extensions pointing towards time

--- qMath/TimeSpanOperations.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace qMath {
-	public static class TimeSpanOperations {
-		public static DateTime AsDateTime(this TimeSpan span) => new DateTime(span.Ticks);
-
-		public static DateTime AsDateTime(this TimeSpan span, DateTimeKind kind) => new DateTime(span.Ticks, kind);
-	}
-}
